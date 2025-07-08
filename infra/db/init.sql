38d2fd779258eadafe4f64a9da938e7e58079595
duls-27 <ing.simonedolci@gmail.com>
2025-07-08 11:18:53 +0200

    Correzzione tabella Amminsitratori passowrd not required

--- infra/db/init.sql
+++ infra/db/init.sql
@@ -99,7 +99,7 @@ CREATE TABLE amministratori (
   cognome VARCHAR(100) NOT NULL,
   email VARCHAR(150) NOT NULL,
   username VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(255),
   creato_il TIMESTAMP DEFAULT NOW()
 );
 
